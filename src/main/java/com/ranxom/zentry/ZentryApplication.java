fdc78c928ee11e2ce26a9bb74f82e1393142c512
RanXom <saiyedshizain@gmail.com>
2026-03-05 16:06:21 +0530

    project initialization

---
+++ src/main/java/com/ranxom/zentry/ZentryApplication.java
@@ -0,0 +1,13 @@
+package com.ranxom.zentry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZentryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZentryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ranxom/zentry/ZentryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ranxom.zentry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZentryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
