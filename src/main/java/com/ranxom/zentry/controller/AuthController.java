513494697d7331b47b413cfc9aeb468310a69cf1
RanXom <saiyedshizain@gmail.com>
2026-03-07 23:20:26 +0530

    introduce controller, securityconfig and permission entity

---
+++ src/main/java/com/ranxom/zentry/controller/AuthController.java
@@ -0,0 +1,22 @@
+package com.ranxom.zentry.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+
+    @PostMapping("/register")
+    public ResponseEntity<String> register() {
+        return ResponseEntity.status(HttpStatus.OK).body("User Registered");
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<String> authenticate() {
+        return ResponseEntity.status(HttpStatus.OK).body("User Authenticated");
+    }
+}
---
+++ src/main/java/com/ranxom/zentry/model/Permission.java
@@ -0,0 +1,24 @@
+package com.ranxom.zentry.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "permissions")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Permission {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "permission_id")
+    private Long id;
+
+    @Column(name = "permission_key", unique = true, nullable = false)
+    private String key;
+
+    private String description;
+}
