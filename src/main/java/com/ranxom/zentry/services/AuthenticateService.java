0d8d999bc1206c8296694eb3d55e50daba4b873a
RanXom <saiyedshizain@gmail.com>
2026-03-08 00:22:01 +0530

    introduce register and login (authenticate) services

---
+++ src/main/java/com/ranxom/zentry/services/AuthenticateService.java
@@ -0,0 +1,45 @@
+package com.ranxom.zentry.services;
+
+import com.ranxom.zentry.dto.AuthenticationRequest;
+import com.ranxom.zentry.dto.AuthenticationResponse;
+import com.ranxom.zentry.repository.UserRepository;
+import com.ranxom.zentry.security.JwtService;
+import com.ranxom.zentry.security.ZentryUserDetails;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthenticateService {
+
+    private final UserRepository repository;
+    private final JwtService jwtService;
+    private final AuthenticationManager authenticationManager;
+
+    public AuthenticateService(
+            UserRepository repository,
+            JwtService jwtService,
+            AuthenticationManager authenticationManager
+    ) {
+        this.jwtService = jwtService;
+        this.repository = repository;
+        this.authenticationManager = authenticationManager;
+    }
+
+    public AuthenticationResponse execute(AuthenticationRequest request) {
+        // This triggers the full Spring Security Auth Provider check
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        request.getUsername(),
+                        request.getPassword()
+                )
+        );
+
+        var user = repository.findByUsername(request.getUsername())
+                .orElseThrow();
+        var userDetails = new ZentryUserDetails(user);
+        var jwtToken = jwtService.generateToken(userDetails);
+        return AuthenticationResponse.builder().token(jwtToken).build();
+    }
+
+}
---
+++ src/main/java/com/ranxom/zentry/services/RegisterService.java
@@ -0,0 +1,43 @@
+package com.ranxom.zentry.services;
+
+import com.ranxom.zentry.dto.AuthenticationResponse;
+import com.ranxom.zentry.dto.RegisterRequest;
+import com.ranxom.zentry.model.User;
+import com.ranxom.zentry.repository.UserRepository;
+import com.ranxom.zentry.security.JwtService;
+import com.ranxom.zentry.security.ZentryUserDetails;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RegisterService {
+
+    private final UserRepository repository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtService jwtService;
+
+    public RegisterService(
+            UserRepository repository,
+            PasswordEncoder passwordEncoder,
+            JwtService jwtService
+    ) {
+        this.repository = repository;
+        this.passwordEncoder = passwordEncoder;
+        this.jwtService = jwtService;
+    }
+
+    public AuthenticationResponse execute(RegisterRequest request) {
+        var user = User.builder()
+                .username(request.getUsername())
+                .email(request.getEmail())
+                .password(passwordEncoder.encode(request.getPassword())) // BCrypting
+                .active(true)
+                .build();
+        repository.save(user);
+
+        var userDetails = new ZentryUserDetails(user);
+        var jwtToken = jwtService.generateToken(userDetails);
+        return AuthenticationResponse.builder().token(jwtToken).build();
+    }
+
+}
